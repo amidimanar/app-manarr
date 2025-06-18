16470347b6fffd4c26b469ee1a2ac47ca6494cf7
amidimanar <mbarkimanar82@gmail.com>
2025-06-18 23:59:50 +0100

    Update settings.py

--- backend/agrovera_microservices/auth_service/auth_service/settings.py
+++ backend/agrovera_microservices/auth_service/auth_service/settings.py
@@ -25,6 +25,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 ROOT_URLCONF = 'auth_service.urls'
 TEMPLATES = [{
